ddd236992248b2fc255ec2b22e863c3352d94c94
AVDambeck <84113969+AVDambeck@users.noreply.github.com>
2021-05-15 15:21:43 -0400

    Update main.py
    is now functional, as well as having device choosing and listing

--- main.py
+++ main.py
@@ -2,25 +2,43 @@
 print("this is a first draft. Midi off has not been sent. Only play terminal sounds or have a midi panic ready")
 
 #import
-import evdev
-import rtmidi
-import time
-import sys
+import evdev, rtmidi, time
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import eventlist
 
 #establish midi port
-midiout = rtmidi.MidiOut()
-midiout.open_virtual_port()
+midiout = rtmidi.MidiOut(name="MoteMidi")
+midiout.open_virtual_port("events-out")
 
-#initiate gamepad
-print("Select /dev/input/event number")
-gamepadLoc = "/dev/input/event" + str(input())
-gamepad = evdev.InputDevice(gamepadLoc)
-print("selected " + str(gamepad))
+#declairing variables
+run = True
+gamepad = ""
+longPrompt = True
+
+#define gamepad function
+def getGamepad(test):
+    global run
+    global gamepad
+    global longPrompt
+    try:
+        gamepad = evdev.InputDevice("/dev/input/event" + str(test))
+    except:
+        if longPrompt == True:
+            print("Could not access input device /dev/input/event" + str(test) + " You might not have permisson or it might not exist. Try another.")
+            longPrompt = False
+        else:
+            print("Could not access input device./dev/input/event" + str(test))
+    else:
+        print('#' * 80)
+        print(gamepad)
+#        print("press q to quit")
+        print('#' * 80)
+        run = False
 
 
-
-#class for each physical button
-class Input(object):
+#define class for each physical button
+class Control(object):
     def __init__(self, code, type, message="debug", note="1"):
         #type 0 = dummy, 1 = midi, 2 = controls
         self.code = code
@@ -33,31 +51,53 @@ class Input(object):
         #midi - play slef.note
         if self.type == 1:
             #if the pressure isnt 0 play note
-            if val == 1:
+            if val != 0:
                 print(self.message + str(val))
                 note_on = [0x90, self.note, 64]
                 midiout.send_message(note_on)
             else:
                 pass
         elif self.type == 2:
-            print(self.message + str(val))
+            pass
 
         else:
             print(self.message + str(val))
 
+
 # define all the notes
 
-PadGreen = Input(18, 1, "green", 45)
-PadBlue = Input(17, 1, "blue", 56)
-PadRed = Input(16, 1, "red", 38)
-PadYellow = Input(20, 1, "yello", 42)
-PadOrange = Input(21, 1, "orange", 64)
-Pedal = Input(22, 1, "pedal", 36)
-StickX = Input(0, 2, "StickX", 1)
-StickY = Input(1, 2, "StickY", 2)
+PadGreen = Control(18, 1, "green", 45)
+PadBlue = Control(17, 1, "blue", 40)
+PadRed = Control(16, 1, "red", 38)
+PadYellow = Control(20, 1, "yello", 42)
+PadOrange = Control(21, 1, "orange", 61)
+Pedal = Control(22, 1, "pedal", 36)
+StickX = Control(0, 2, "StickX", 1)
+StickY = Control(1, 2, "StickY", 2)
 
-lsInput = [PadGreen, PadBlue, PadRed, PadYellow, PadOrange, StickX, StickY, Pedal]
+lsControl = [PadGreen, PadBlue, PadRed, PadYellow, PadOrange, StickX, StickY, Pedal]
 
+
+
+#START
+
+#get event numbers
+print('#' * 80)
+print("""Select /dev/input/event number (enter "list" to scan)""")
+print('#' * 80)
+while run == True:
+    num = input()
+
+    #cycle exception
+    if num == "list":
+        eventlist.list()
+        print
+        print('#' * 80)
+        print("""Select /dev/input/event number""")
+        print('#' * 80)
+
+    else:
+        getGamepad(num)
 
 #main
 for event in gamepad.read_loop():
@@ -66,11 +106,11 @@ for event in gamepad.read_loop():
         print(event)
     elif event.type == evdev.ecodes.EV_ABS:
         #if event.value !=0:
-        for i in lsInput:
+        for i in lsControl:
             if event.code == i.code:
                 i.payload(event.value)
 
 
-
-print("this was supposed to stop the program :/")
+del gamepad
+del midiout
 sys.exit()

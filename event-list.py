91d3f1ef0eb6ca8dc904499c9881ceeb217ad6dc
AVDambeck <84113969+AVDambeck@users.noreply.github.com>
2021-05-14 22:51:24 -0400

    Command line tool for listing
    To list all devices, you need sudo, but any device you want to use for mid your user probobly already has access to

---
+++ event-list.py
@@ -0,0 +1,35 @@
+#Command line tool to list events
+from evdev import InputDevice, categorize, ecodes
+import os
+
+dev = ""
+longPrompt = True
+
+#gamepad function
+def getGamepad(test):
+    global dev
+    global longPrompt
+    try:
+        dev = InputDevice("/dev/input/event" + str(test))
+    except:
+        if longPrompt == True:
+            print("Could not access input device /dev/input/event" + str(test) + " You might not have permisson or it might not exist. Try another.")
+            longPrompt = False
+        else:
+            print("Could not access input device./dev/input/event" + str(test))
+    else:
+        print(dev)
+
+#get highest event
+for i in os.listdir("/dev/input"):
+    if "event" in i:
+      max = int(i.replace("event", ""))
+      break
+
+#list events
+for i in range(1,max):
+    getGamepad(i)
+
+#exit
+del dev
+exit()
